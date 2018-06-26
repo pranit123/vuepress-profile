82a9c9260c830bc7f99e96eee29c1e8af6cd80e7
Praneet Rohida <praneetrohida@gmail.com>
2018-06-27 00:16:02 +0530

    Set up VuePress

---
+++ src/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title: 'Praneet Rohida',
+  description: 'Just playing around',
+  dest: './.vuepress/dist',
+  serviceWorker: true,
+  themeConfig: {
+    home: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Connect',
+        items: [
+          { text: 'Twitter', link: 'https://twitter.com/praneetrohida' },
+          { text: 'Github', link: 'https://github.com/pranit123' },
+          {
+            text: 'LinkedIn',
+            link: 'https://www.linkedin.com/in/praneet-rohida'
+          },
+          {
+            text: 'Email',
+            link: 'mailto:praneetrohida@gmail.com'
+          }
+        ]
+      }
+    ],
+    sidebar: ['/', '/projects/', ['/intro/', 'Work Experience']]
+  }
+}
---
+++ src/README.md
@@ -0,0 +1,19 @@
+---
+{
+    "title": "Praneet Rohida",
+    "lang": "en-US",
+    home: true,
+    actionText: "More about me",
+    actionLink: "/projects/",
+    features: [
+        {
+            title: "Developer",
+              details: "Devvv"
+        }
+    ]
+}
+---
+# Hello
+
+
+
